05d51f31edcad6a6f66803961534ceac435b8f98
suyytib <152405015+suyytib@users.noreply.github.com>
2024-03-20 22:46:08 +0800

    登录

---
+++ 在线学习/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/register')
+def register():
+    # return render_template('register.html')
+    return render_template('temp.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5200, debug=False)
