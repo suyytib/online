37ee6441da4d4f0a8805efe322fd44eed0a1760c
607163943 <133568451+607163943@users.noreply.github.com>
2024-03-20 22:51:35 +0800

    temp

--- 在线学习/app.py
+++ 在线学习/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/register')
+@app.route('/register'
 def register():
     # return render_template('register.html')
     return render_template('temp.html')
