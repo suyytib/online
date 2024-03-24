464b7f4f909801f2edb9865c674b524f1488d82a
suyytib <152405015+suyytib@users.noreply.github.com>
2024-03-24 13:18:59 +0800

    Delete FlaskWeb改 directory

--- FlaskWeb改/blueprints/model.py
+++
@@ -1,9 +0,0 @@
-from blueprints.table_config import db
-# 用户表
-class User(db.Model):
-    # 创建的数据库表名
-    __tablename__="user"
-    username=db.Column(db.Integer,primary_key=True,autoincrement=True)
-    password=db.Column(db.String(200),nullable=False)
-    email=db.Column(db.String(255),nullable=False)
-    
