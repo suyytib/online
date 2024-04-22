dca6c14f54a119f304f19fc18cfd93e20b67cfa2
suyytib <152405015+suyytib@users.noreply.github.com>
2024-04-22 21:08:17 +0800

    Delete blueprints directory

--- blueprints/root.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, request, url_for
-from flask import render_template
-
-bp=Blueprint("root",__name__,url_prefix="/")
-@bp.route('/')
-def root():
-    return render_template('root.html')
