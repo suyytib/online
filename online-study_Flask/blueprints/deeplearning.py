052a47f05a10b01a016a632c057050f18d9e78a6
suyytib <152405015+suyytib@users.noreply.github.com>
2024-04-26 16:29:50 +0800

    Update deeplearning.py

--- online-study_Flask/blueprints/deeplearning.py
+++ online-study_Flask/blueprints/deeplearning.py
@@ -7,11 +7,20 @@ from model import Comment
 # import tensorflow as tf
 bp=Blueprint("deeplearning",__name__,url_prefix="/deeplearning")
 @bp.route('/')
-@captcha__is_login
 def deeplearning_root():
     return render_template("/deeplearning.html")
 
-@bp.route('/A/<A_id>')
-def all_a(A_id):
-    comment = Comment.query.filter(Comment.tieba_id == A_id)
-    return render_template(f'/deeplearning/A{A_id}.html', comment=comment)
+@bp.route('/A1')
+def A1():
+    comment = Comment.query.filter(Comment.tieba_id == 1)
+    return render_template(f'/deeplearning/A1.html', comment=comment)
+
+@bp.route('/A2')
+def A2():
+    comment = Comment.query.filter(Comment.tieba_id == 2)
+    return render_template(f'/deeplearning/A2.html', comment=comment)
+
+@bp.route('/A3')
+def A3():
+    comment = Comment.query.filter(Comment.tieba_id == 3)
+    return render_template(f'/deeplearning/A3.html', comment=comment)
