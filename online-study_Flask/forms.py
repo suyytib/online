2f7c22565d7c914506de47180cab1856a5f2759e
suyytib <152405015+suyytib@users.noreply.github.com>
2024-05-02 21:24:05 +0800

    Update forms.py

--- online-study_Flask/forms.py
+++ online-study_Flask/forms.py
@@ -1,8 +1,8 @@
 from wtforms import Form, StringField,PasswordField
-from wtforms.validators import Length,Email,EqualTo,InputRequired
+from wtforms.validators import Length,Email,EqualTo,InputRequired,regexp
 # 参与注册验证的表单类
 class Register_Form(Form):
-    username = StringField(validators=[InputRequired(u"用户名不能为空"),Length(min=4,max=20)])
+    username = StringField(validators=[InputRequired(u"用户名不能为空"),Length(min=4,max=20),regexp('^[A-Za-z][A-Za-z0-9_.]*$',0,'name is invalid')])
     passwd =PasswordField(validators=[InputRequired(u"密码不能为空"),Length(min=6,max=20)])
     email=StringField(validators=[InputRequired(u"邮件不能为空"),Email()])
     re_password = PasswordField(validators=[InputRequired(u"密码不能为空"),EqualTo("passwd",u"两次密码不一致")])
